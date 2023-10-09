05c2130635c438f9c9c38a0e1fd8d1461de45399
Ken Lai <ken.mercus.lai@gmail.com>
2023-10-09 20:45:19 +1000

    Breaking the Vigenère Cipher

--- 3. Breaking the Vigenère Cipher/solution.py
+++ 3. Breaking the Vigenère Cipher/solution.py
@@ -1,2 +1,8 @@
+from itertools import cycle
+
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def get_keyword(ciphertext, key_len):
+    splited_ciphertext = [ciphertext[i:i+key_len] for i in range(0, len(ciphertext), key_len)]
+    print(splited_ciphertext)
     return
