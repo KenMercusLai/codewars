819c845d60ded2502541f4abbb6e76284ca8c67d
Ken Lai <ken.mercus.lai@gmail.com>
2023-10-09 21:45:31 +1000

    Breaking the Vigenère Cipher

--- 3. Breaking the Vigenère Cipher/solution.py
+++ 3. Breaking the Vigenère Cipher/solution.py
@@ -1,8 +1,21 @@
 from itertools import cycle
 
-ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+CIPHER = "CODEWARS"
+
 
 def get_keyword(ciphertext, key_len):
-    splited_ciphertext = [ciphertext[i:i+key_len] for i in range(0, len(ciphertext), key_len)]
-    print(splited_ciphertext)
+    splitted_ciphertext = [
+        ciphertext[i : i + key_len] for i in range(0, len(ciphertext), key_len)
+    ]
+    print(splitted_ciphertext)
+
+    for i in splitted_ciphertext:
+        for j, v in enumerate(i):
+            new_index = ALPHABET.index(v) - ALPHABET.index(CIPHER[j])
+            if new_index < 0:
+                new_index += 26
+            print(
+                ALPHABET[new_index],
+            )
     return
